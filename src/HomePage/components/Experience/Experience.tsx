3606afe9c4b206bcb8d1f4c7b9145fb40f1bbb77
Mariano-SI <mariano.silva@ufvjm.edu.br>
2026-01-17 18:59:49 -0300

    fix: atualizar URLs das fotos das empresas na seção de experiência

--- src/HomePage/components/Experience/Experience.tsx
+++ src/HomePage/components/Experience/Experience.tsx
@@ -19,7 +19,7 @@ function Experience() {
         jobDescription: 'Atuo em modelo outsourcing na PagoNxt, parte do grupo Santander, desenvolvendo IDPs (Internal Developer Portals) que aprimoram a organização do ecossistema de tecnologia e aumentam a eficiência dos profissionais de T.I das empresas do grupo. Meu trabalho envolve desenvolvimento backend e frontend, CI/CD e práticas de DevOps, contribuindo para automação, padronização e otimização dos fluxos de desenvolvimento. Entre as tecnologias que utilizo estão Node.js, TypeScript, Docker, Azure e Backstage.io. Além de desenvolver, participo ativamente do planejamento técnico, propondo soluções que reduzem tempo de entrega e melhoram a experiência dos times de engenharia.',
         date: '01/2025 - 11/2025',
         skills: ['Node.js', 'Azure', 'Docker', 'BackstageIO'],
-        companyPhotos:['https://media.licdn.com/dms/image/v2/C4D0BAQHxuL9Y3B6-Ug/company-logo_200_200/company-logo_200_200/0/1660926816605/ilegra_logo?e=1756944000&v=beta&t=vUJJaW_HcKlo0QSqzA6PAOS14V8ZD2LTBvm6Cn6Lh9s', 'https://media.licdn.com/dms/image/v2/D4D0BAQFgl6M-ZPv0Dw/company-logo_200_200/company-logo_200_200/0/1707392681527/pagonxt_logo?e=1756944000&v=beta&t=oCK7UZZViQ0gWFUmp7lH5AqLcyOosg80QFvzfkSTrSM']
+        companyPhotos:['https://media.licdn.com/dms/image/v2/D4E0BAQHBqA4a7zHOSA/company-logo_200_200/B4EZtHBFGVKUAI-/0/1766423025107/ilegra_logo?e=1770249600&v=beta&t=zZCLPI16MYIeS5i4isRTJBwSf4DAUlLLhpNxeGnprC8', 'https://media.licdn.com/dms/image/v2/D4D0BAQFgl6M-ZPv0Dw/company-logo_200_200/company-logo_200_200/0/1707392681527/pagonxt_logo?e=1770249600&v=beta&t=KfCPN05sa54BHixNyBIe9zIVRNlbrFyNilb1mCOw00Y']
     },
     {
         companyName: 'Dti Digital',
@@ -27,7 +27,7 @@ function Experience() {
         jobDescription: 'Atuei liderando a equipe responsável pelo desenvolvimento da plataforma interna Round. Minhas atividades incluíam o desenvolvimento de novas funcionalidades, atuação como referência técnica para o time, realização de code reviews e gerenciamento dos lançamentos semanais de releases. Nesse período, liderei a refatoração do modelo de e-commerce da plataforma e participei ativamente do planejamento e desenvolvimento de uma feature completa para mapeamento de habilidades dos funcionários (skill mapping).',
         date: '07/2024 - 01/2025',
         skills: ['CI/CD', 'React', 'Node.js', 'Infraestrutura', 'Cloud computing', 'Docker', 'Redis', 'Liderança de equipe', 'Planejamento de features',  'TypeScript', 'SQL', 'Docker', 'Git', 'Jest', 'Cypress'],
-        companyPhotos: ['https://media.licdn.com/dms/image/v2/D4E0BAQHDojXmC8wClw/company-logo_200_200/B4EZY1JXKDG0AQ-/0/1744648369262/dtidigital_logo?e=1756944000&v=beta&t=AXV8IElHa_rfhx9J87M53eNlwIUCRKHRq9lGx37smSM']
+        companyPhotos: ['https://media.licdn.com/dms/image/v2/D4E0BAQHDojXmC8wClw/company-logo_200_200/B4EZY1JXKDG0AQ-/0/1744648369262/dtidigital_logo?e=1770249600&v=beta&t=uk6dZZ_TTJZrUDM1deBRqgHAVgrB8n7H0tZ6y9mtzV8']
     },
     {
         companyName: 'Dti Digital',
@@ -35,7 +35,7 @@ function Experience() {
         date: '04/2024 - 07/2024',
         jobDescription: 'Atuei principalmente no desenvolvimento de software utilizando as tecnologias Node e React.js juntamente ao Typescript. Além disso, em certos momentos atuei como desenvolvedor líder iterino como preparação para assumir o cargo de líder da equipe no futuro.',
         skills:['React', 'Node.js', 'TypeScript', 'SQL', 'Docker', 'Git', 'Jest', 'Cypress'],
-        companyPhotos: ['https://media.licdn.com/dms/image/v2/D4E0BAQHDojXmC8wClw/company-logo_200_200/B4EZY1JXKDG0AQ-/0/1744648369262/dtidigital_logo?e=1756944000&v=beta&t=AXV8IElHa_rfhx9J87M53eNlwIUCRKHRq9lGx37smSM']
+        companyPhotos: ['https://media.licdn.com/dms/image/v2/D4E0BAQHDojXmC8wClw/company-logo_200_200/B4EZY1JXKDG0AQ-/0/1744648369262/dtidigital_logo?e=1770249600&v=beta&t=uk6dZZ_TTJZrUDM1deBRqgHAVgrB8n7H0tZ6y9mtzV8']
     },
     {
         companyName: 'Dti Digital',
@@ -43,7 +43,7 @@ function Experience() {
         jobDescription: 'Atuei principalmente no desenvolvimento de software utilizando as tecnologias Node e React.js juntamente ao Typescript. Além disso, auxiliva minhas lideranças no planejamento de novas features do projeto e também, quando necessário, contribia com a equipe de produto nos processos de discovery que aconteciam frequentemente em meu contexto, algo bastante positivo pois me incentivava a sempre estar aprendendo novas coisas e assumir novas responsabilidades dentro da equipe.',
         date: '09/2022 - 05/2024',
         skills: ['React', 'Node.js', 'TypeScript', 'SQL', 'Docker', 'Git', 'Jest', 'Cypress'],
-        companyPhotos: ['https://media.licdn.com/dms/image/v2/D4E0BAQHDojXmC8wClw/company-logo_200_200/B4EZY1JXKDG0AQ-/0/1744648369262/dtidigital_logo?e=1756944000&v=beta&t=AXV8IElHa_rfhx9J87M53eNlwIUCRKHRq9lGx37smSM']
+        companyPhotos: ['https://media.licdn.com/dms/image/v2/D4E0BAQHDojXmC8wClw/company-logo_200_200/B4EZY1JXKDG0AQ-/0/1744648369262/dtidigital_logo?e=1770249600&v=beta&t=uk6dZZ_TTJZrUDM1deBRqgHAVgrB8n7H0tZ6y9mtzV8']
     },
     {
         companyName: 'Empresa Júnior Next Step',
@@ -51,7 +51,7 @@ function Experience() {
         jobDescription: 'Membro da NextStep, empresa júnior do curso de sistemas desde meu primeiro ano de faculdade. No início de 2022 tive a oportunidade de assumir o papel de diretor da empresa, uma experiência extremamente positiva que que refinou drasticamente a maneira na qual me enxergo, me posiciono e participo de uma equipe.',
         date: '08/2020 - 02/2023',
         skills: ['Liderança', 'HTML', 'CSS', 'Javascript', 'Wordpress'],
-        companyPhotos:['https://media.licdn.com/dms/image/v2/C4E0BAQEq1ujMToBwJw/company-logo_200_200/company-logo_200_200/0/1676327270056/nextstepsi_logo?e=1756944000&v=beta&t=48gqrCK6mxOqLNw5T1rYwUVvMH77udrmksipaWMGpgs']
+        companyPhotos:['https://media.licdn.com/dms/image/v2/C4E0BAQEq1ujMToBwJw/company-logo_200_200/company-logo_200_200/0/1676327270056/nextstepsi_logo?e=1770249600&v=beta&t=mii6Eti13BCLPZkgZ1igerg1c1ZASQccGOZ-rPUXDUE']
     },
     {
         companyName: 'Secretaria de Meio-Ambiente - SEMAD MG',
@@ -59,7 +59,7 @@ function Experience() {
         jobDescription: 'Como Estagiário de TI na Secretaria de Estado de Meio-Ambiente e Desenvolvimento Sustentável (SEMAD), desenvolvi habilidades em treinamento de usuários e digitalização de documentos.',
         date: '06/2020 - 08/2022',
         skills: ['Comunicação assertiva', 'HTML', 'CSS', 'Javascript'],
-        companyPhotos:['https://media.licdn.com/dms/image/v2/C4E0BAQG1bJHTCdjrMA/company-logo_200_200/company-logo_200_200/0/1635119757543?e=1756944000&v=beta&t=Qq8WImq4d2uXFWW0oF368yqnsRundIlxqE92HH6omCo']
+        companyPhotos:['https://media.licdn.com/dms/image/v2/C4E0BAQG1bJHTCdjrMA/company-logo_200_200/company-logo_200_200/0/1635119757543?e=1770249600&v=beta&t=CM1IbAc_rM01M3MNqzcE06EKgRlf7clmk6ADRkl1ITY']
     },
     
   ]  
